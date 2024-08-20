746a0efc92c2d4420dbc11aab24753d94c0902c9
Arslan <arslanhaider.dev@hotmail.com>
2024-08-20 13:38:37 +0500

    add chatbot controller

---
+++ controller.py
@@ -0,0 +1,9 @@
+class ChatbotController:
+    def get_response(self, query: str):
+        """
+        Gets response from the e-commerce chatbot
+
+        :param query: user query
+        :return: llm response
+        """
+        pass
