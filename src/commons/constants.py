6cd046d0bf17f08d648368fd8110fa82d83e22f4
Arslan <arslanhaider.dev@hotmail.com>
2024-08-21 16:52:19 +0500

    add initial prompt

--- src/commons/constants.py
+++ src/commons/constants.py
@@ -3,3 +3,8 @@ from enum import Enum
 
 class OpenAiModel(Enum):
     GPT_35_TURBO = "gpt-3.5-turbo"
+
+
+CHAT_BOT_INITIAL_PROMPT = """
+You are an intelligent e-commerce assistant. Answer the user's queries about products, pricing, availability, and more.
+"""
