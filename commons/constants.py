6ac56f2af9c206e6f904752671eb3c5b970f2e60
Arslan <arslanhaider.dev@hotmail.com>
2024-08-21 20:34:03 +0500

    change placeholder message

--- commons/constants.py
+++ commons/constants.py
@@ -1,4 +1,4 @@
 CHATBOT_NAME = "## Costco Assistant"
-USER_QUERY_PLACEHOLDER = "Type your query and press enter"
+USER_QUERY_PLACEHOLDER = "Type your query and press enter..."
 GRADIO_CSS = "./static/css/chat_window.css"
 GRADIO_TITLE_ELEMENT_ID = "title"
