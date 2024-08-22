13c747300c3d24d3e20543db3f39a5adbaae3e55
Arslan <arslanhaider.dev@hotmail.com>
2024-08-22 14:05:15 +0500

    formatting

--- commons/factory/llms.py
+++ commons/factory/llms.py
@@ -5,7 +5,7 @@ def llm_factory(llm_identifier: str):
     """
     factory to get the llm using the identifier
 
-    :param llm_identifier: LLM identifier example: openai, gemini etc
+    :param llm_identifier: LLM identifier example: openai, gemini etc.
     :return: LLM class
     """
     llms = {
