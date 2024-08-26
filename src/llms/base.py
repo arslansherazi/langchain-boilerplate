22f52c7df363b6fd446dd990993f4fa45912a83d
Arslan <arslanhaider.dev@hotmail.com>
2024-08-26 12:41:48 +0500

    update doc strings

--- src/llms/base.py
+++ src/llms/base.py
@@ -1,12 +1,24 @@
+"""
+Base module for all LLMs
+"""
 from langchain_core.messages import HumanMessage
 
 
 class BaseLLM:
+    """
+    Base class for all LLMs
+    """
     def __init__(self, model_name: str):
         self.__model_name = model_name
         self.__llm = None
 
     def get_response(self, query: str) -> str:
+        """
+        Get response from the LLM
+
+        :param query: user query
+        :return: llm response
+        """
         messages = [
             HumanMessage(content=query)
         ]
@@ -15,4 +27,9 @@ class BaseLLM:
         return results_data
 
     def get_llm(self):
+        """
+        Get the llm object
+
+        :return: llm object
+        """
         return self.__llm
--- src/llms/openai.py
+++ src/llms/openai.py
@@ -1,3 +1,6 @@
+"""
+OpenAi LLM module
+"""
 from langchain_openai import ChatOpenAI
 
 from src.llms.base import BaseLLM
@@ -5,13 +8,22 @@ from src.commons.constants import OpenAiModel
 
 
 class OpenAiLLM(BaseLLM):
+    """
+    OpenAi LLM class
+    """
     def __init__(self, model_name: str):
         super().__init__(model_name)
         self.__initialize_llm()
 
     def __verify_model_name(self):
+        """
+        Verify the model name. If the model name is not valid, set the default model name.
+        """
         if not OpenAiModel.has_value(self.__model_name):
             self.__model_name = OpenAiModel.GPT_35_TURBO.value
 
     def __initialize_llm(self):
+        """
+        Initialize the OpenAi LLM
+        """
         self.__llm = ChatOpenAI(model=OpenAiModel.GPT_35_TURBO.value)
